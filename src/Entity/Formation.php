9549eaf47eeee457545fcfeb9651911311561e30
Bret Quentin <qbret@iutbayonne.univ-pau.fr>
2022-01-11 13:55:33 +0100

    Creation BD

---
+++ src/Entity/Formation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FormationRepository::class)
+ */
+class Formation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nomCourt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCourt(): ?string
+    {
+        return $this->nomCourt;
+    }
+
+    public function setNomCourt(string $nomCourt): self
+    {
+        $this->nomCourt = $nomCourt;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Stage.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nomCourt;
+
+    /**
+     * @ORM\Column(type="string", length=300)
+     */
+    private $nomLong;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCourt(): ?string
+    {
+        return $this->nomCourt;
+    }
+
+    public function setNomCourt(string $nomCourt): self
+    {
+        $this->nomCourt = $nomCourt;
+
+        return $this;
+    }
+
+    public function getNomLong(): ?string
+    {
+        return $this->nomLong;
+    }
+
+    public function setNomLong(string $nomLong): self
+    {
+        $this->nomLong = $nomLong;
+
+        return $this;
+    }
+}
