9a3924f81ef2717e60f43223c6a0dd54fc3c99be
Bret Quentin <qbret@iutbayonne.univ-pau.fr>
2022-01-11 14:23:49 +0100

    Migration BD

---
+++ migrations/Version20220111131537.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220111131537 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE entreprise (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(300) NOT NULL, adresse VARCHAR(300) NOT NULL, activite VARCHAR(300) NOT NULL, site_web VARCHAR(300) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, nom_court VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stage (id INT AUTO_INCREMENT NOT NULL, nom_court VARCHAR(100) NOT NULL, nom_long VARCHAR(300) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE entreprise');
+        $this->addSql('DROP TABLE formation');
+        $this->addSql('DROP TABLE stage');
+    }
+}
