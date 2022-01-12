ccc2bcff7eabc660682c0f3dee4f83a9a0547aee
Bret Quentin <qbret@iutbayonne.univ-pau.fr>
2022-01-12 15:07:46 +0100

    Maj donnees

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,20 +13,15 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker= \Faker\Factory::create('fr_FR');
-        // $product = new Product();
-        // $manager->persist($product);
-        
-        
-        //
-        //DECLARATION DES DIFFERENTES FORMATIONS
-        //
+    
+        // Gestion Entité Formation:
 
-        $formations=array();//liste compléte des formations
+        $listeFormation=array();
 
         $listeNomCourt = array("DUT info","DUT Design","DUT Art","Bac S","Bac L","Licence Bio");
         $listeNomLong = array("Diplôme Universitaire Technologique Informatique","Diplôme Universitaire Technologique Design","Diplôme Universitaire Technologique Art","Baccalauréat Scientifique","Baccalauréat Littéraire","Licence Biologie");
         
-        for($i=0;$i<8;$i++)//on génére aléatoirement des formations
+        for($i=0;$i<12;$i++)
         {   
             $nomCourt=$listeNomCourt[$faker->numberBetween(0,count($listeNomCourt)-1)];
             $nomLong=$listeNomLong[$faker->numberBetween(0,count($listeNomLong)-1)];
@@ -36,21 +31,21 @@ class AppFixtures extends Fixture
             $uneFormation->setNomCourt($nomCourt);
             $uneFormation->setNomLong($nomLong);
 
-            array_push($formations,$uneFormation);
+            array_push($listeFormation,$uneFormation);
 
             $manager->persist($uneFormation);
 
         }
         
 
-        //
-        //DECLARATIONS DES ENTREPRISES
-        //
+       
+        // Gestion Entité Entreprise:
+
         $listeActivite=array("sportive","billard","prog web", "dev site","aviation","service","artisan");
 
-        $entreprises= array();//liste compléte des entreprises
+        $listeEntreprise= array();
 
-        for($i=1;$i<8;$i++)
+        for($i=1;$i<12;$i++)
         {
             $uneEntreprise=new Entreprise();
 
@@ -59,61 +54,32 @@ class AppFixtures extends Fixture
             $uneEntreprise->setActivite($listeActivite[$faker->numberBetween(0,count($listeActivite)-1)]);
             $uneEntreprise->setSiteWeb("https://".$uneEntreprise->getNom().".com");
 
-            array_push($entreprises,$uneEntreprise);
+            array_push($listeEntreprise,$uneEntreprise);
 
             $manager->persist($uneEntreprise);
         }
 
 
-        //
-        //DECLARATION DES STAGES
-        //
+        // Gestion Entité Stage:
+
         $code = array("java","c","c++","sql","javascript","php","html");
 
-        for($i=1;$i<15;$i++)//boucle de generation de stage
+        for($i=1;$i<23;$i++)
         {
             $unStage= new Stage();
-
-            //Generation des variables aleatoires
             
             $stageCode=$code[$faker->numberBetween(0,count($code)-1)];
             $titre="Stage en ".$stageCode;
 
             $unStage->setTitre($titre);
-            $unStage->setMission($faker->word); //A changer
+            $unStage->setMission($faker->word); 
             $unStage->setEmail($faker->email());
 
-            $unStage->setEntreprise($entreprises[$faker->numberBetween(0,count($entreprises)-1)]);
+            $unStage->setEntreprise($listeEntreprise[$faker->numberBetween(0,count($listeEntreprise)-1)]);
 
-            $nombreFormation=$faker->numberBetween(1,count($formations)-1);
+            $nombreFormation=$faker->numberBetween(1,count($listeFormation)-1);
 
-            $unStage->addFormation($formations[$nombreFormation]);
-
-            // for($j=0;$j<=$nombreFormation;$j++)//boucle d'ajout des formations
-            // {
-            //     $boolean="FALSE";
-
-            //     $formationTiree=$faker->numberBetween(0,count($formations)-1);
-
-            //     for($z=0;$z<count($formationsDejaSelectionnees);$z++)//boucle de verification des formations deja ajoutées
-            //     {
-            //         if($formationsDejaSelectionnees[$z]==$formationTiree)
-            //         {
-            //             $boolean = "TRUE";
-            //         }
-            //     }
-
-            //     if($boolean=="FALSE")
-            //     {
-            //         array_push($formationsDejaSelectionnees,$formationTiree);
-            //         $stage->addFormation($formations[$formationTiree]);
-            //     }
-            //     else
-            //     {
-            //         $y--;
-            //     }
-
-            // }
+            $unStage->addFormation($listeFormation[$nombreFormation]);
 
             $manager->persist($unStage);
         }
