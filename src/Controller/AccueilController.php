fe488ec93b83baa8f5e8d60e62ec4c0176f62abe
Bret Quentin <qbret@iutbayonne.univ-pau.fr>
2022-01-13 11:41:01 +0100

    Mise en place des première fonctionnalités

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
+
+
 class AccueilController extends AbstractController
 {
     /**
@@ -39,12 +44,13 @@ class AccueilController extends AbstractController
     }
 
     /**
-     * @Route("/stages/{id}", name="stages")
+     * @Route("/stages", name="stages")
      */
-    public function stage($id): Response
+    public function stage(): Response
     {
+        $stages= $this->getDoctrine()->getRepository(Stage::class)->findAll();
         return $this->render('accueil/stage.html.twig', [
-            'controller_name' => 'StagesController', 'idRessource'=>$id
+            'controller_name' => 'StagesController', 'liste_stages'=>$stages
         ]);
     }
 }
